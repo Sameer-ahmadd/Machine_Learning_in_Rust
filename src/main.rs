4791e463f4117a5ecf3a3a6db0618ad867908791
Sameer-ahmadd <samimughal7786@gmail.com>
2025-01-29 18:39:02 +0500

    Loaded the Dataframe from disk into memory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use house_price_predictor::{download_csv_file, load_csv_file};
+
 // Training Script entry point...
 // steps
 // 1. Download the CSV file to the disk
@@ -13,22 +15,9 @@ fn main() -> anyhow::Result<()> {
     // 1. Download the CSV file to the disk
     let csv_file_path = download_csv_file()?;
 
+    // 2. Load the Data from disk into memory
+
+    let df = load_csv_file(&csv_file_path)?;
+
     Ok(())
-}
-
-// Download the CSV file to the disk
-fn download_csv_file() -> anyhow::Result<String> {
-    let url: &str = "https://github.com/selva86/datasets/blob/master/BostonHousing.csv";
-    // GET the response from the URL
-    let response = reqwest::blocking::get(url)?;
-
-    // GET the bytes from the response into the memory
-    let bytes = response.bytes()?;
-
-    let file_path = "boston_housing.csv";
-
-    // Write the bytes to the disk
-    std::fs::write(file_path, bytes)?;
-
-    Ok(file_path.to_string())
 }
