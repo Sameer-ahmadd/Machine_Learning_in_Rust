cee66d338b78821c451e790cb24eb3ba1611b3d8
Sameer-ahmadd <samimughal7786@gmail.com>
2025-02-05 12:01:31 +0500

    Added Predict Endpoint to accept data as a JSON Payload

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -1,5 +1,6 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use log::{info, warn};
+use serde::Deserialize;
 
 // Health check endpint
 // Returns an 200 Ok response if the API is healthy.
@@ -10,6 +11,31 @@ async fn health() -> impl Responder {
     HttpResponse::Ok().body("I am healthy!")
 }
 
+#[derive(Deserialize, Debug)]
+struct PredictRequest {
+    crim: f64,
+    zn: f64,
+    indus: f64,
+    chas: f64,
+    nox: f64,
+    rm: f64,
+    age: f64,
+    dis: f64,
+    rad: f64,
+    tax: f64,
+    ptratio: f64,
+    b: f64,
+    lstat: f64,
+}
+
+#[post("/predict")]
+async fn predict(payload: web::Json<PredictRequest>) -> impl Responder {
+    info!("Predict health point called");
+    info!("Features sent by the client:{:?}", payload);
+
+    HttpResponse::Ok().body("Prediction")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Initialize logger
@@ -18,7 +44,7 @@ async fn main() -> std::io::Result<()> {
 
     info!("Starting the API...");
 
-    HttpServer::new(|| App::new().service(health))
+    HttpServer::new(|| App::new().service(health).service(predict))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await?;
